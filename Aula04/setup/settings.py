30a70321593201ddf3af4121ebfe0dcbde83ff3b
Puffer Notebook <marceloballinflores@gmail.com>
2026-01-26 20:28:42 -0300

    .

--- Aula04/setup/settings.py
+++ Aula04/setup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m3)*q#51htjs@*29vqr93z(e*&gk(hvfs=8v(ss7ca-w21bnau'
+SECRET_KEY = 'django-insecure-f_0sp$yu^qjh4n=qococ+w3ysfzt4a8*m6srer1zz-x8lgs-_9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
