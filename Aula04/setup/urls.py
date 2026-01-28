469133daed0a837c99d8c02cee033d4bd6b3a74d
Puffer Notebook <marceloballinflores@gmail.com>
2026-01-28 19:31:34 -0300

    .

--- Aula04/setup/urls.py
+++ Aula04/setup/urls.py
@@ -1,3 +1,4 @@
+
 """
 URL configuration for setup project.
 
@@ -16,9 +17,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import home, listar, criar, fechar # Importamos a função que criamos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home),  # Deixando vazio '', a página aparece na raiz do site
+    path('listar', listar), # Página inicial lista os chamados
+    path('novo/<str:lab>/<str:problema>/<str:prioridade>/', criar), # Rota dinâmica
+    path('fechar/<int:indice>', fechar), # Rota dinâmica
 ]
