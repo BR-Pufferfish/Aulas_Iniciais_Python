a9c2e508c1409ab34f4e7fc69465ebbd2c01f7ab
Puffer Notebook <marceloballinflores@gmail.com>
2026-02-06 21:52:03 -0300

    .

--- Aula04/core/admin.py
+++ Aula04/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Chamado, Categoria, Equipamentos, Pessoa
+
 # Register your models here.
+admin.site.register(Chamado)
+admin.site.register(Categoria)
+admin.site.register(Equipamentos)
+admin.site.register(Pessoa)
