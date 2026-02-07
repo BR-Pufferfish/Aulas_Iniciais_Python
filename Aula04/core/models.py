bcb04f209c15a4671bc99494a40c5d3de59409b6
Puffer Notebook <marceloballinflores@gmail.com>
2026-02-06 21:13:54 -0300

    .

--- Aula04/core/models.py
+++ Aula04/core/models.py
@@ -47,3 +47,30 @@ class Equipamentos(models.Model):
 
     def __str__(self):
         return f"{self.descricao} - {self.tipo}"
+    
+
+class Pessoa(models.Model):
+    cpf = models.IntegerField()
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=15)
+    endereco = models.CharField(max_length=200)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+    cep = models.CharField(max_length=10)
+    genero = models.CharField(max_length=20)
+    data_nascimento = models.DateField()
+    OPCOES_CARGO = [
+        ('Cliente', 'Cliente'),
+        ('Funcionário', 'Funcionário'),
+        ('Administrador', 'Administrador'),
+    ]
+    condicao = models.CharField(max_length=50, choices=OPCOES_CARGO, default='Cliente')
+    OPCOES_SITUACAO = [
+        ('Ativo', 'Ativo'),
+        ('Inativo', 'Inativo'),
+    ]
+    situacao = models.CharField(max_length=50, choices=OPCOES_SITUACAO, default='Ativo')
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.cpf} - {self.nome}"
