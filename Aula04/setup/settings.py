036d834adac146c230448aff94e24fda72829a76
Puffer Notebook <marceloballinflores@gmail.com>
2026-02-13 21:55:10 -0300

    .

--- Aula04/setup/settings.py
+++ Aula04/setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f_0sp$yu^qjh4n=qococ+w3ysfzt4a8*m6srer1zz-x8lgs-_9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
